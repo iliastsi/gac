2217c199f73f4215d28274bf0329f08aa68c5d09
ilias <iliastsi@hotmail.com>
2011-09-12 17:16:58 +0300

    Rewrite ErrUtils

--- src/main/ErrUtils.hs
+++ src/main/ErrUtils.hs
@@ -1,52 +1,64 @@
 --------------------------------------------------------------------------------
 -- (c) Tsitsimpis Ilias, 2011
 --
+-- Utilities for error reporting
+--
 --------------------------------------------------------------------------------
 
 module ErrUtils (
-    Message, Messages, emptyMessages,
-    mkWarnMsg, mkErrMsg, snocBag,
-    mkLocWarnMsg, mkLocErrMsg
+    Message, mkLocMessage,
+    msgSpan, msgContext, msgExtraInfo,
+
+    MsgCode,
+
+    ErrMsg, WarnMsg,
+    ErrorMessages, WarningMessages,
+    Messages, errorsFound, emptyMessages,
+    mkErrMsg, mkWarnMsg,
   ) where
 
+import Bag
 import SrcLoc
 
-type Bag a = [a]
 
-type Message = String
+-- -------------------------------------------------------------------
+-- Basic error codes
+
+data MsgCode
+  = UnknownErr
+
+-- -------------------------------------------------------------------
+-- Collecting up messages for later ordering and printing
+
+data Message = Msg {
+    msgSpan         :: SrcSpan,
+    msgContext      :: MsgCode,
+    msgExtraInfo    :: String
+    }
+
+mkLocMessage :: SrcSpan -> MsgCode -> String -> Message
+mkLocMessage msgSpan msgContext msgExtraInfo =
+    Msg msgSpan msgContext msgExtraInfo
+
+-- An error message
+type ErrMsg  = Message
+
+mkErrMsg :: SrcSpan -> MsgCode -> String -> ErrMsg
+mkErrMsg = mkLocMessage
+
+-- A warning message
 type WarnMsg = Message
-type ErrMsg = Message
+
+mkWarnMsg :: SrcSpan -> MsgCode -> String -> WarnMsg
+mkWarnMsg = mkLocMessage
 
 type Messages = (Bag WarnMsg, Bag ErrMsg)
+
+type WarningMessages = Bag WarnMsg
+type ErrorMessages   = Bag ErrMsg
 
 emptyMessages :: Messages
 emptyMessages = (emptyBag, emptyBag)
 
-emptyBag :: Bag a
-emptyBag = []
-
-mkWarnMsg :: String -> WarnMsg
-mkWarnMsg msg = mkMsg ("Warning: " ++ msg)
-
-mkErrMsg :: String -> ErrMsg
-mkErrMsg msg = mkMsg ("Error: " ++ msg)
-
-mkMsg :: String -> Message
-mkMsg msg = msg
-
-snocBag :: Bag Message -> Message -> Bag Message
-snocBag bag msg = bag ++ [msg]
-
-mkLocWarnMsg :: SrcLoc -> String -> WarnMsg
-mkLocWarnMsg pos msg = mkLocMsg pos ("Warning: " ++ msg)
-
-mkLocErrMsg :: SrcLoc -> String -> ErrMsg
-mkLocErrMsg pos msg = mkLocMsg pos ("Error: " ++ msg)
-
-mkLocMsg :: SrcLoc -> String -> Message
-mkLocMsg loc msg =
-    let lin     = getSrcLine loc
-        col     = getSrcColumn loc
-        loc_msg = show lin ++ ':' : show col
-    in
-    loc_msg ++ ": " ++ msg
+errorsFound :: Messages -> Bool
+errorsFound (warns, errs) = not (isEmptyBag errs)
