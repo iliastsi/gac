995d49aa64182c41a2814f0655606ffdbc6de57b
ilias <iliastsi@hotmail.com>
2011-10-19 14:04:01 +0300

    Add preprocessor to Setup.hs to run ppCpp before ppHappy for Parser

--- Setup.hs
+++ Setup.hs
@@ -1,4 +1,18 @@
 #!/usr/bin/env runhaskell
 
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.PreProcess
+import System.FilePath
+
+main = defaultMainWithHooks
+        simpleUserHooks{hookedPreProcessors=[("ypp",ppYpp)]}
+
+ppYpp build local =
+    PreProcessor {
+        platformIndependent = True,
+        runPreProcessor =
+            mkSimplePreProcessor $ \inFile outFile verbosity -> do
+                let yFile = replaceExtension outFile "y"
+                runSimplePreProcessor (ppCpp   build local) inFile yFile   verbosity
+                runSimplePreProcessor (ppHappy build local) yFile  outFile verbosity
+    }
